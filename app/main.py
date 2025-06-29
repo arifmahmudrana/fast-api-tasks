44ee10a8f66539d8ea6de95fe852febaccef082d
Arif Mahmud Rana <amrana83@gmail.com>
2025-06-29 14:35:45 +0600

    add lifespan app connect to mongo, apply indexing, disconnect on app shutdown add tasks to app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
-from .routers import users
+from contextlib import asynccontextmanager
+from .routers import users, tasks
+from .mongo import connect_to_mongo, disconnect_from_mongo, ensure_indexes
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application startup")
+    await connect_to_mongo()
+    await ensure_indexes()
+    yield
+    await disconnect_from_mongo()
+    print("Application shutdown")
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(users.router)
+app.include_router(tasks.router)
