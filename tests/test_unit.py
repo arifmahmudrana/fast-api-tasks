4699e924cb429a20a993e0e0078a7037cba2329d
Arif Mahmud Rana <amrana83@gmail.com>
2025-06-29 21:02:55 +0600

    delete tests/test_unit.py

--- tests/test_unit.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from app.crud import create_access_token
-
-
-def test_create_access_token():
-    data = {"sub": "test@example.com"}
-    token = create_access_token(data)
-    assert isinstance(token, str)
-    assert len(token) > 0
